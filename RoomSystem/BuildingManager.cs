4a4d47da275e3c60c3e9dfe3a6b2831b80aeb184
JeremiasKoenig <jeremias.koenig@outlook.com>
2023-10-01 15:50:55 +0200

    Changes to Atlas and Buildables

--- RoomSystem/BuildingManager.cs
+++ RoomSystem/BuildingManager.cs
@@ -5,51 +5,73 @@ using System.Linq;
 public class BuildingManager
 {
     private readonly Main main;
+    private readonly Node buildableContainer;
 
     public BuildingManager(Main main)
     {
         this.main = main;
-        Build();
+        buildableContainer = main.GetNode("Buildable");
     }
 
     public IEnumerable<Buildable> BuildableMachines { get; } = new[]
     {
-        // Generates power for a full room
-        new Buildable("Generator", false, "", new[] { (ResourceType.BuildingMaterials, 20), (ResourceType.DuctTape, 10), (ResourceType.Tools, 10) }),
+        // generates power for a full room
+        new Buildable("Generator", false,
+            "Generator",
+            "A generator that can power an additional room", 
+            new[] { (ResourceType.BuildingMaterials, 20), (ResourceType.Circuitry, 10), (ResourceType.Tools, 10) }),
 
-        // Needed to power up a room
-        new Buildable("OxygenGenerator", false, "", new[] { (ResourceType.BuildingMaterials, 2), (ResourceType.DuctTape, 3), (ResourceType.Tools, 2) }),
+        // needed to power up or down a room
+        new Buildable("OxygenGenerator", false,
+            "Oxygen Sealer",
+            "A generator to provide it's room with oxygen",
+            new[] { (ResourceType.BuildingMaterials, 2), (ResourceType.DuctTape, 3), (ResourceType.Chemicals, 2) }),
             
-        // Can be used to power up/down a room
-        new Buildable("PowerSwitch", false, "", new[] { (ResourceType.Circuitry, 3), (ResourceType.Tools, 2) }),
+        // story goal 1
+        new Buildable("ComputerSystem", true,
+            "Computer System",
+            "The computer system of the station, as long as it is down large parts of the station are locked.",
+            new[] { (ResourceType.Circuitry, 15), (ResourceType.DuctTape, 5), (ResourceType.Tools, 15) }),
+        
+        // story goal 2
+        new Buildable("Antenna", true,
+            "Comm Antenna",
+            "An antenna of the communication system. All antennas must be restored before an emergency signal can be sent.",
+            new[] { (ResourceType.Circuitry, 5), (ResourceType.Tools, 5) }),
 
-        new Buildable("ComputerSystem", true, "", new[] { (ResourceType.Circuitry, 15), (ResourceType.DuctTape, 5), (ResourceType.Tools, 15) }),
-        new Buildable("Antenna", true, "", new[] { (ResourceType.Circuitry, 10), (ResourceType.Tools, 5) }),
-        new Buildable("FuelPump", true, "", new[] { (ResourceType.BuildingMaterials, 15), (ResourceType.DuctTape, 10), (ResourceType.Tools, 5), (ResourceType.Chemicals, 20) }),
+        // story goal 3 and 4
+        new Buildable("FuelPump", true,
+            "Fuel Pump",
+            "A fuel pump to refuel a landed spacecraft. All pumps need to be restored to refill the escape shuttle.",
+            new[] { (ResourceType.BuildingMaterials, 5), (ResourceType.DuctTape, 10), (ResourceType.Tools, 5), (ResourceType.Chemicals, 5) }),
     };
 
-    void Build()
+    public void Build(Buildable buildable, Vector2 position)
     {
-        var node = InstantiateBuildable(BuildableMachines.ToArray()[0]);
-        node.GlobalPosition = new Vector2(48, 48);
+        var node = InstantiateBuildable(buildable);
+        node.GlobalPosition = position;
         node.Visible = true;
-        main.AddChild(node);
+        buildableContainer.AddChild(node);
     }
 
     Node2D InstantiateBuildable(Buildable buildable)
     {
-        return main.GetNode<Node2D>($"BuildingPrototypes/{buildable.Name}").Duplicate() as Node2D;
+        return main.GetNode<Node2D>($"BuildingPrototypes/{buildable.Key}").Duplicate() as Node2D;
     }
 
     public class Buildable
     {
-        public string Name { get; }
+        public string Key { get; }
         public bool Repair { get; }
+        public string Description { get; }
+        public IEnumerable<(ResourceType type, int amount)> Cost { get; }
 
-        public Buildable(string name, bool repair, string description, IEnumerable<(ResourceType type, int amount)> cost)
+        public Buildable(string key, bool repair, string name, string description, IEnumerable<(ResourceType type, int amount)> cost)
         {
-            Name = name;
+            Key = key;
             Repair = repair;
+            Description = description;
+            Cost = cost;
         }
     }
 }
